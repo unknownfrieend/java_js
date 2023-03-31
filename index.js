20052955fabca4277b3cf2be392c825323aaf579
unknownfrieend <unknownfrieend@gmail.com>
2023-03-31 20:12:05 +0500

    sasa

---
+++ index.js
@@ -0,0 +1,19 @@
+2
+
+function num(a, b) {
+    if (a == 6 || b == 6) {
+        console.log(true);
+    } else if (a + b == 6) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
+num(4, 2)
+
+4
+
+let name = +prompt("Ism kiriting")
+
+console.log("Hello " + name + " how are you");
